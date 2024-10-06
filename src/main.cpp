ab0dcde2b1bf3699e8d66457f6baf58909fc247a
WindustH <windusth@qq.com>
2024-10-06 16:41:56 +0800

    Added patch for PNG files with invalid end "AE 42";Fixed some issues

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <codec.hpp>
 #include <iostream>
 #include <stdexcept>
+#include <fstream>
+#include <iomanip>
 
 const int OPERATE_ENCRYPT = 0;
 const int OPERATE_DECRYPT = 1;
@@ -10,6 +12,7 @@ const int TYPE_PC = 1;   // 像素混淆
 const int TYPE_RPC = 2;  // 行像素混淆
 const int TYPE_PER = 3;  // PicEncrypt 行混淆
 const int TYPE_PERC = 4; // PicEncrypt 行+列混淆
+
 const std::string HELP_INFO=
 R"(
 PicConfusion Codec by Windust
@@ -51,6 +54,54 @@ Note:
 5. The output image (-o) can be any path where you want to save the encrypted/decrypted result.
 )";
 
+bool AppendBytesIfEndsWithAE42(const std::string& filename) {
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+    if (!file.is_open()) {
+        std::cerr << "Unable to open file: " << filename << std::endl;
+        return false;
+    }
+
+    // Move to the end of the file
+    std::streampos fileSize;
+    file.seekg(0, std::ios::end);
+    fileSize = file.tellg();
+    file.seekg(-2, std::ios::end);
+
+    // Check if the file has at least 2 bytes
+    if (fileSize < 2) {
+        std::cerr << "File is too small to have 2 bytes at the end." << std::endl;
+        file.close();
+        return false;
+    }
+
+    // Read the last two bytes
+    unsigned char lastTwoBytes[2] = {0, 0};
+    file.read(reinterpret_cast<char*>(lastTwoBytes), 2);
+    file.close();
+
+    // Output the last two bytes in hexadecimal format
+    std::cout << "Last two bytes in hexadecimal: 0x" 
+              << std::hex << std::uppercase << (int)lastTwoBytes[0] 
+              << " 0x" << std::hex << std::uppercase << (int)lastTwoBytes[1] 
+              << std::endl;
+
+    // Check if they are AE 42
+    if (lastTwoBytes[0] == 0xAE && lastTwoBytes[1] == 0x42) {
+        std::ofstream outFile(filename, std::ios::binary | std::ios::app);
+        if (!outFile.is_open()) {
+            std::cerr << "Unable to open file to append: " << filename << std::endl;
+            return false;
+        }
+
+        // Append 60 82
+        outFile.write("\x60\x82", 2);
+        outFile.close();
+        return true;
+    }
+
+    return false;
+}
+
 cv::Mat run(const int &operation, const int &type, const cv::Mat &image,const std::string &key, int blockSizeX, int blockSizeY)
 {
     if (operation == OPERATE_ENCRYPT)
@@ -64,7 +115,7 @@ cv::Mat run(const int &operation, const int &type, const cv::Mat &image,const st
         case TYPE_PC:
             return encryptPC(image, key);
         case TYPE_RPC:
-            return encryptPC(image, key);
+            return encryptRPC(image, key);
         case TYPE_PER:
             return encryptPER(image, key);
         case TYPE_PERC:
@@ -84,7 +135,7 @@ cv::Mat run(const int &operation, const int &type, const cv::Mat &image,const st
         case TYPE_PC:
             return decryptPC(image, key);
         case TYPE_RPC:
-            return decryptPC(image, key);
+            return decryptRPC(image, key);
         case TYPE_PER:
             return decryptPER(image, key);
         case TYPE_PERC:
@@ -150,6 +201,11 @@ int main(int argc, char *argv[])
             i++;
             key=std::string(argv[i]);
         }
+    }
+    if (AppendBytesIfEndsWithAE42(input_path)) {
+        std::cout << "File has been updated." << std::endl;
+    } else {
+        std::cout << "File does not end with AE 42, no update performed." << std::endl;
     }
     cv::Mat input_image=cv::imread(input_path, cv::IMREAD_COLOR);
     cv::Mat output_image=run(operation,type,input_image,key,blockSizeX,blockSizeY);
